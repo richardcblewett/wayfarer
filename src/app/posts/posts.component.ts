2b3c2d669a4d8f9328541da254740af1ba5b17d3
Seth Thomas <athomas.seth@gmail.com>
2022-01-21 16:32:57 -0500

    Building out posts option

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { POSTS } from './posts';
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
+
 })
 export class PostsComponent implements OnInit {
 
   //loren ipsem generated stuff?
-  title: string = '';
-  text: string = '';
-  date: number = 0;
-  city: string = ''; //one of the cities
-
-  constructor() { }
+  
+  post: any;
+  posts= POSTS;
+  
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.post = POSTS.find(post => {
+        let paramId: string = params.get("id") || "";
+        return post.id === parseInt(paramId);
+      });
+    });
   }
 
 }
