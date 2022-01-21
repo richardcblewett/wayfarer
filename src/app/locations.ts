2ac405a6296a283f02e61a4b382f48378d74d665
Richard Blewett <richardcblewett@gmail.com>
2022-01-21 13:44:28 -0500

    added links to default city images

--- src/app/locations.ts
+++ src/app/locations.ts
@@ -1,6 +1,6 @@
 export const locations = [
-    { id: 1, name: "San Francisco", image: "image" },
-    { id: 2, name: "London", image: "image" },
-    { id: 3, name: "Seattle", image: "image" },
-    { id: 4, name: "Sydney", image: "image" },
+    { id: 1, name: "San Francisco", image: "https://images.pexels.com/photos/208745/pexels-photo-208745.jpeg" },
+    { id: 2, name: "London", image: "https://images.pexels.com/photos/50632/pexels-photo-50632.jpeg" },
+    { id: 3, name: "Seattle", image: "https://images.pexels.com/photos/3964406/pexels-photo-3964406.jpeg" },
+    { id: 4, name: "Sydney", image: "https://images.pexels.com/photos/2845013/pexels-photo-2845013.jpeg" },
 ];
