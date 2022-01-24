4bff6a67e32d360d90f45126cbfa6c2d9df8df0b
Seth Thomas <athomas.seth@gmail.com>
2022-01-24 11:50:22 -0500

    Clicking on Title Moves to Post page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,16 @@ import { CitiesComponent } from './cities/cities.component';
 import { HomeComponent } from './home/home.component';
 import { PostsComponent } from './cities/posts/posts.component';
 
+
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "cities/:id", component: CitiesComponent },
-  { path: "posts/:id", component: PostsComponent },
+  { path: "cities/:id/posts/:id", component: PostsComponent}
+  
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
