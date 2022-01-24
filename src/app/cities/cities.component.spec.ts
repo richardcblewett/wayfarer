1bed6bf6337e94e57c0a2edbf36b59d29fa2b06a
Richard Blewett <richardcblewett@gmail.com>
2022-01-24 17:06:34 -0500

    the carousel now uses city images.

--- src/app/cities/cities.component.spec.ts
+++ src/app/cities/cities.component.spec.ts
@@ -1,25 +1,35 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
 import { CitiesComponent } from './cities.component';
+import {RouterTestingModule} from "@angular/router/testing";
+import {Location} from "@angular/common";
+import {Router} from "@angular/router";
 
 describe('CitiesComponent', () => {
   let component: CitiesComponent;
   let fixture: ComponentFixture<CitiesComponent>;
+  let router:Router
+  let location:Location;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ CitiesComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
     fixture = TestBed.createComponent(CitiesComponent);
+    router.initialNavigation();
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    //fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('navigate to "/2" takes you to london', fakeAsync(() => {
+    router.navigate(["cities/2"]).then(() => {
+      expect(location.path()).toBe("cities/2");
+    });
+  }));
 });
