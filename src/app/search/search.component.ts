48830af9cad2b36a18e12d63adda7ef28b72bb65
Richard Blewett <richardcblewett@gmail.com>
2022-01-25 11:34:25 -0500

    added the ability to search posts for specific words in titles and the post | removed the searching from the header because that was causing problems and added the searching to the navigation section | added city ids to the posts | added cards for the posts returned from the search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,23 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { Subject } from 'rxjs';
+import {Component, OnInit} from '@angular/core';
+import {POSTS} from "../cities/posts/posts";
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
+
 export class SearchComponent implements OnInit {
 
+  posts = POSTS;
   search: string = '';
+  matchingPosts:any = [];
+
   constructor() { }
 
   ngOnInit(): void {
   }
-  // When this is connected to an database this will allow you to search for posts
+  //When this is connected to a database this will allow you to search for posts
   findPost(search: string){
-    {
-      console.log(search);
-    }
+    this.matchingPosts = [];
+    //console.log(search);
+    this.posts.forEach(post => {
+      if (post.title.includes(search) || post.description.includes(search)) {
+        this.matchingPosts.push(post);
+      }
+    });
+    //console.log(this.matchingPosts);
   }
 }
